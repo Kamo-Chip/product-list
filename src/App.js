7effc3399ed82d52e448d9b777779167c0ed74ff
Kamogelo Khumalo <KamoKhumalo04@gmail.com>
2022-08-29 21:27:03 +0200

    Bug fix

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
           <Route
             path="/"
             element={
-              <ProductList products={products} setProducts={setProducts} />
+              <ProductList products={products} setProducts={setProducts} fetchProducts={fetchProducts}/>
             }
           />
           <Route
